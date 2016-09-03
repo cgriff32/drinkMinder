520eee24e64668c1b6e4577c38d70ffaf9004d3d
cgriff32 <cgriff32@gmail.com>
2016-09-03 18:01:08 +0200

    Update Drink.ino

--- Drink.ino
+++ Drink.ino
@@ -2,35 +2,9 @@
 #include <TimeLib.h>
 #include <avr/sleep.h>
 
-/*
-  Button
-
- Turns on and off a light emitting diode(LED) connected to digital
- pin 13, when pressing a pushbutton attached to pin 2.
-
-
- The circuit:
- * LED attached from pin 13 to ground
- * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
-
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
-
-
- created 2005
- by DojoDave <http://www.0j0.org>
- modified 30 Aug 2011
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/Button
- */
-
 // constants won't change. They're used here to
 // set pin numbers:
-const int buttonPin = 3;     // the number of the pushbutton pin (Pin 2 on ATtiny)
+const int switchPin = 3;     // (Pin 2 on ATtiny)
 const int redPin = 0;       // (Pin 5 on ATtiny)
 const int greenPin = 1;     // (Pin 6 on ATtiny)
 const int bluePin = 4;      // (Pin 3 on ATtiny)
@@ -41,50 +15,52 @@ volatile int drinkAlarm = 3;           //1 = manually turned off, 0 = automatica
 
 void setup() {
   // initialize the LED pin as an output:
-  pinMode(13, OUTPUT);
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
+  
   //Setup interrupt on pin 2
   GIMSK = 0b00100000;    // turns on pin change interrupts
   PCMSK = 0b00000100;    // turn on interrupts on pin 7 (PB3)
   sei();                 // enables interrupts
   
-  LEDCheck();
+  LEDCheck();            // Display welcome led sequence
 }
 
 void loop() {
+  // Since hour timer will roll over at 23 to 0, make sure we account for the difference in timing
   if (currentTime >= 3570){
     currentTime = currentTime - 3600;
   }
   
-int difference = ((hour() * 60) + minute()) - currentTime;
+  // find elapsed time since last action
+  int difference = ((hour() * 60) + minute()) - currentTime;
 
   //  Detect drink, start timer for 30 minutes
-  if (drinkAlarm == 2){
-    LEDCheck();
-    alarmOff();
-    drinkAlarm = 1;
+  if (drinkAlarm == 2){ // This means last action was a drink
+    LEDCheck();         // Confirm drink
+    alarmOff();         // Turn lights off, reset timer
+    drinkAlarm = 1;     // 1 means alarm was manually turned off
     
-  }else if(drinkAlarm == 1){
-    if (difference >= 60){
-      alarmOff();
-      drinkAlarm = 0;
+  }else if(drinkAlarm == 1){  // Last action involved alarm being manually turned off
+    if (difference >= 35){    // Light has been on for 5 minutes
+      alarmOff();             // Turn drink alarm off, user won't be taking a drink
+      drinkAlarm = 0;         // 0 means alarm was automatically turned off
 
-      startSleep();
+      startSleep();           // Set the device in sleep mode, user must be away from drink
       
-      alarmOff();
+      alarmOff();             // Device has returned from sleep mode, reset timer.
       
-    }else if(difference >= 30){
-      rainbowLED();
+    }else if(difference >= 30){ // 30 minutes since last drink
+      rainbowLED();             // Let's see some colors (drink alarm on)
     }
   }else{
-    startSleep();
+    startSleep();   // This occurs when the device was turned off automatically by being left alone for too long
   }
 }
 
 ISR(PCINT0_vect){
-  drinkAlarm = 2;
+  drinkAlarm = 2;   // Interrupt used to return from sleep mode. The user must have taken a drink
 }
 
 void startSleep(){
@@ -104,10 +80,11 @@ void startSleep(){
 }
 
 void alarmOff(){
-  setColourRgb(0, 0, 0);
-  currentTime = (hour() * 60) + minute();
+  setColourRgb(0, 0, 0);                  // turn off the LED
+  currentTime = (hour() * 60) + minute(); // Start a new timer, measured in minutes
 }
 
+//Used to write a value to each pin of the RGB LED
 void setColourRgb(unsigned int red, unsigned int green, unsigned int blue) {
   analogWrite(redPin, red);
   analogWrite(greenPin, green);
@@ -115,7 +92,7 @@ void setColourRgb(unsigned int red, unsigned int green, unsigned int blue) {
  }
  
 void rainbowLED(){
-    unsigned int rgbColour[3] = {255, 0, 0};
+    unsigned int rgbColour[3] = {255, 0, 0};  // Start at red
 
   // Choose the colours to increment and decrement.
   for (int decColour = 0; decColour < 3; decColour += 1) {
@@ -123,6 +100,11 @@ void rainbowLED(){
 
     // cross-fade the two colours.
     for(int i = 0; i < 255; i += 1) {
+      if (drinkAlarm == 2){ //The user drank, let's prematurely end this loop
+        i = 255;
+      }
+      
+      //Cycle through those colors
       rgbColour[decColour] -= 1;
       rgbColour[incColour] += 1;
       
@@ -132,6 +114,7 @@ void rainbowLED(){
   }
 }
 
+//  Blink red, green, blue as a welcome and confirmation message
 void LEDCheck(){
   setColourRgb(255,0,0);
   delay(500);
